cb879bca8356e3c2f88d33238fb460539cdc2b26
George <greg@gregs-MacBook-Pro.local>
2019-11-01 16:27:44 -0700

    clean html, readme, and js

--- README.md
+++ README.md
@@ -9,22 +9,22 @@
 1. Spec: The program returns a range of numbers from 0 to the users inputted number
 Input: "4"
 Output: "0, 1, 2, 3, 4"
-COMPLETE
+
 
 2. Spec: The program replaces numbers that contain a "1" with "Beep!"
 Input: "12"
 Output: "Beep!"
-COMPLETE
+
 
 3. Spec: the program replaces numbers that contain "2" with "Boop!"
 Input: 2
 Output: "Boop!"
-COMPLETE
+
 
 4. Spec: the program replaces numbers that contain "3" with "I'm sorry, Dave. I'm afraid I can't do that."
 Input: 33
 Output: "I'm sorry, Dave. I'm afraid I can't do that."
-COMPLETE
+
 
 5. Spec: the program includes all the elements  "Beep!", "Boop!","I'm sorry, Dave. I'm afraid I can't do that." for numbers that dont include 1 or 2 or 3.
 Input: 5
--- js/scripts.js
+++ js/scripts.js
@@ -3,91 +3,87 @@
 // MY FUNCTION NAME
 function converter(number) {
 
-// MY VARIABLES TO CALL ON FUNCTIONS
-var words = ["I'm sorry, Dave. I'm afraid I can't do that.", "Boop!", "Beep!", "test"]
-var keyNumbers = ["3", "2", "1"]
-var convertedNumbers ="";
-var myGreaterFive = [" 0"," Beep!", " Boop!", " I'm sorry, Dave. I'm afraid I can't do that."]
+  // MY VARIABLES TO CALL ON FUNCTIONS
+  var words = ["I'm sorry, Dave. I'm afraid I can't do that.", "Boop!", "Beep!", "test"]
+  var keyNumbers = ["3", "2", "1"]
+  var convertedNumbers ="";
+  var myGreaterFive = [" 0"," Beep!", " Boop!", " I'm sorry, Dave. I'm afraid I can't do that."]
 
-// RETURN THE WORD IN REGARDS TO ITS SPECIFIC number
-// THIS WILL RETURN I'm sorry, Dave. I'm afraid I can't do that WHEN NUMBER 3 IS ENTERED
-for (i=0; i<number.length; i++) {
-  if ( number === "3") {
-    convertedNumbers = convertedNumbers + words[0];
-    return convertedNumbers;
-  }
-  // THIS WILL RETURN BOOP WHEN NUMBER 2 IS ENTERED
-  if ( number === "2") {
-    convertedNumbers = convertedNumbers + words[1];
-    return convertedNumbers;
-  }
-  // THIS WILL RETURN BEEP WHEN 1 IS ENTERED
-  if ( number === "1") {
-    convertedNumbers = convertedNumbers + words[2];
-    return convertedNumbers;
-  }
-};
-// THIS FOR LOOP WILL DETECT IF ANY OF THE keyNumbers ARRAY ELEMENTS ARE INCLUDED IN THE INPUT NUMBERS
-for (i=0; i<number.length; i++) {
-  // THIS WILL RETURN I'm sorry, Dave. I'm afraid I can't do that WHEN NUMBER 3 IS INCLUDED IN THE ENTERED NUMBER
-  if (number.includes(keyNumbers[0])) {
-    convertedNumbers = convertedNumbers + words[0];
-    return convertedNumbers;
-  }
+
+
+// WILL POTENTIAL REMOTE BELOW FIRTS SECTION SINCE SPECS WILL BE COVERED BY SECTION 2
+
+
+// SECTION 1
+  // RETURN THE WORD IN REGARDS TO ITS SPECIFIC number
+  // THIS WILL RETURN I'm sorry, Dave. I'm afraid I can't do that WHEN NUMBER 3 IS ENTERED
+  for (i=0; i<number.length; i++) {
+    if ( number === "3") {
+      convertedNumbers = convertedNumbers + words[0];
+      return convertedNumbers;
+    }
+
+    // THIS WILL RETURN BOOP WHEN NUMBER 2 IS ENTERED
+    if ( number === "2") {
+      convertedNumbers = convertedNumbers + words[1];
+      return convertedNumbers;
+    }
+
+    // THIS WILL RETURN BEEP WHEN 1 IS ENTERED
+    if ( number === "1") {
+      convertedNumbers = convertedNumbers + words[2];
+      return convertedNumbers;
+    }
+  };
+
+
+// SECTION 2
+  // THIS FOR LOOP WILL DETECT IF ANY OF THE keyNumbers ARRAY ELEMENTS ARE INCLUDED IN THE INPUT NUMBERS
+  for (i=0; i<number.length; i++) {
+    // THIS WILL RETURN I'm sorry, Dave. I'm afraid I can't do that WHEN NUMBER 3 IS INCLUDED IN THE ENTERED NUMBER
+    if (number.includes(keyNumbers[0])) {
+      convertedNumbers = convertedNumbers + words[0];
+      return convertedNumbers;
+    }
+
     // THIS WILL RETURN BOOP WHEN NUMBER 2 IS INCULDED IN THE ENTERED NUMBER
-  if (number.includes(keyNumbers[1])) {
-    convertedNumbers = convertedNumbers + words[1];
-    return convertedNumbers;
-  }
-  // THIS WILL RETURN BEEP WHEN NUMBER 1 IS INCULDED IN THE ENTERED NUMBER
-  if (number.includes(keyNumbers[2])) {
-    convertedNumbers = convertedNumbers + words[2];
-    return convertedNumbers;
-  }
+    if (number.includes(keyNumbers[1])) {
+      convertedNumbers = convertedNumbers + words[1];
+      return convertedNumbers;
+    }
+
+    // THIS WILL RETURN BEEP WHEN NUMBER 1 IS INCULDED IN THE ENTERED NUMBER
+    if (number.includes(keyNumbers[2])) {
+      convertedNumbers = convertedNumbers + words[2];
+      return convertedNumbers;
+    }
+  };
+
+
+// NOT WORKING AS DESIRED-NEED TO TROUBLESHOOT (SUBTRACTING DOWN FROM ENTERED NUMBER TILL INDEX === 4)
+
+  // SECTION 3
+  // THIS LOOP WILL IDENTIFY WHEN NUMBER ENTERED IS > 5
+  for (var i = 0; i < number.length; i++) {
+    // THIS WILL RETURN "0","Beep!", "Boop!","I'm sorry, Dave. I'm afraid I can't do that." WHEN NUMBER ENTERED IS = 4
+    if (number === "4") {
+      convertedNumbers = convertedNumbers + myGreaterFive + ", " + number[i];
+      return convertedNumbers
+    }
+
+    // THIS WILL RETURN "0","Beep!", "Boop!","I'm sorry, Dave. I'm afraid I can't do that." AND NUMBERS IN BETWEEN UP TILL THE ENTERED VALUE
+    if (number > 4) {
+      var newNumber = parseInt(number) - 1;
+      convertedNumbers = convertedNumbers + myGreaterFive + ", " + newNumber + ", " + number[i];
+      console.log(number);
+      return convertedNumbers;
+    }
+  };
 };
-// THIS LOOP WILL IDENTIFY WHEN NUMBER ENTERED IS > 5
-for (var i = 0; i < number.length; i++) {
-  // THIS WILL RETURN "0","Beep!", "Boop!","I'm sorry, Dave. I'm afraid I can't do that." WHEN NUMBER ENTERED IS = 4
-if (number === "4") {
-  convertedNumbers = convertedNumbers + myGreaterFive + ", " + number[i];
-  return convertedNumbers
-}
-// THIS WILL RETURN "0","Beep!", "Boop!","I'm sorry, Dave. I'm afraid I can't do that." AND NUMBERS INBETWEEN UP TILL THE ENTERED VALUE
-  if (number > 4) {
-  var newNumber = parseInt(number) - 1;
-  convertedNumbers = convertedNumbers + myGreaterFive + ", " + newNumber + ", " + number[i];
-  console.log(number);
-  return convertedNumbers;
-}
-};
-
-
-};
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// USER LOGIC
+// FRONT END LOGIC
 $(document).ready(function() {
   $("form#numbers").submit(function(event) {
     event.preventDefault();
